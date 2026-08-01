f38f583ba12fb1c4872d832bc134bfd30f5993e1
Bahram <bahrammr-apa103@code.edu.az>
2026-08-01 20:35:09 +0400

    tasda

---
+++ backend/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Phonenumber { get; set; }
+    public string Address { get; set; }
+    public string Email { get; set; }
+
+
+
+    public virtual void GetInfo() 
+    {
+        Console.WriteLine($"{Firstname} {Lastname} {Phonenumber} {Address} {Email}");
+
+    }
+
+
+}
+
---
+++ backend/Models/Saler.cs
@@ -0,0 +1,13 @@
+﻿
+public class Saler:Employee
+{
+    public double Salary { get; set; }
+    public double Benefit { get; set; }
+
+    public override void GetInfo()
+    {
+        base.GetInfo();
+        Console.WriteLine($"{Salary} {Benefit}");
+    }
+}
+
